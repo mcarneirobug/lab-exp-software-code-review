ba05de43dc1d2c3554e45abef728a5531e9cc117
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-04-01 22:19:50 -0300

    feat: Created script to write repositories data to json file

---
+++ Sprint 1 - Lab03S01/src/script_to_json.py
@@ -0,0 +1,21 @@
+import json
+import script
+
+
+def write_data_to_json():
+    nodes = script.get_all_repos()
+    print(len(nodes))
+    repos = []
+    for item in nodes:
+        data = {
+            "nameOwner": item["nameWithOwner"],
+            "nameRepository": item["name"],
+            "url": item["url"],
+            "sshUrl": item["sshUrl"],
+            "createdAt": (item["createdAt"])[:10],
+            "prMerged": item["mr"]["totalCount"],
+            "prClosed": item["mc"]["totalCount"]
+        }
+        repos.append(data)
+    with open("repo_data.json", "w") as f:
+        json.dump(repos, f, indent=4)
